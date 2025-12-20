b7604cd324c72b683d917d136916b0f70a2be9b4
jnnaurich <j.aurich@campus.tu-berlin.de>
2025-12-20 14:27:57 +0100

    ..

--- itp/kuhandel.sh
+++ itp/kuhandel.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 file='/home/jnn.aurich/kuhhandel-ml/itp/train_self.py' # adjust to run other script
 
-echo "first  command line parameter: "$1
-echo "second command line parameter: "$2
+# echo "first  command line parameter: "$1
+# echo "second command line parameter: "$2
 
 # activate python environment, make a wrapper for it (otherwise command line arguments are passed to it)
 wrappersource() {
@@ -10,5 +10,5 @@ wrappersource() {
 }
 wrappersource
 # run script with command line arguments (relevant for job arrays) $1=Cluster, $2=Process 
-python3 $file $1 $2
+python3 $file #$1 $2
 exit
